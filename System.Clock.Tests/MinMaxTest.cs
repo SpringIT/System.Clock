546b2b1ac32e8fddb23222c3f0c5b901363faf13
Maarten Mensink <maarten.mensink@spring-it.nl>
2016-03-02 13:28:14 +0100

    Adding Min and Max Methods

---
+++ System.Clock.Tests/MinMaxTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace System.Tests
+{
+    [TestFixture]
+    public class MinMaxTest
+    {
+        [TearDown]
+        public void Setup()
+        {
+            Clock.Reset();
+        }
+
+        [Test]
+        public void Min()
+        {
+            var left = Clock.Today;
+            var right = Clock.Tomorrow;
+
+            var actual = Clock.Min(left, right);
+            actual.Should().Be(left);
+        }
+
+        [Test]
+        public void Max()
+        {
+            var left = Clock.Today;
+            var right = Clock.Tomorrow;
+
+            var actual = Clock.Max(left, right);
+            actual.Should().Be(right);
+        }
+    }
+}
--- System.Clock/Clock.cs
+++ System.Clock/Clock.cs
@@ -1,39 +1,49 @@
 ﻿namespace System
 {
-        public static class Clock
+    public static class Clock
+    {
+        private static readonly Func<DateTime> DefaultDateProvider = () => DateTime.UtcNow;
+
+        public static Func<DateTime> DateTimeProvider = DefaultDateProvider;
+
+        public static void Reset()
         {
-            private static readonly Func<DateTime> DefaultDateProvider = () => DateTime.UtcNow;
-
-            public static Func<DateTime> DateTimeProvider = DefaultDateProvider;
-
-            public static void Reset()
-            {
-                DateTimeProvider = DefaultDateProvider;
-            }
-
-            public static DateTime Now
-            {
-                get { return DateTimeProvider.Invoke(); }
-            }
-
-            public static DateTime Today
-            {
-                get { return Now.Date; }
-            }
-
-            public static DateTime Yesterday
-            {
-                get { return Today.AddDays(-1); }
-            }
-
-            public static DateTime Tomorrow
-            {
-                get { return Today.AddDays(1); }
-            }
-
-            public static long Ticks
-            {
-                get { return Now.Ticks; }
-            }
+            DateTimeProvider = DefaultDateProvider;
         }
+
+        public static DateTime Now
+        {
+            get { return DateTimeProvider.Invoke(); }
+        }
+
+        public static DateTime Today
+        {
+            get { return Now.Date; }
+        }
+
+        public static DateTime Yesterday
+        {
+            get { return Today.AddDays(-1); }
+        }
+
+        public static DateTime Tomorrow
+        {
+            get { return Today.AddDays(1); }
+        }
+
+        public static long Ticks
+        {
+            get { return Now.Ticks; }
+        }
+
+        public static DateTime Min(DateTime left, DateTime right)
+        {
+            return left < right ? left : right;
+        }
+
+        public static DateTime Max(DateTime left, DateTime right)
+        {
+            return left > right ? left : right;
+        }
+    }
 }
